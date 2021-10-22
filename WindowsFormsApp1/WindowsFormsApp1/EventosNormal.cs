7c7234fc989ccfb7931de094a9fae384e87b1fc2
João Paiva <64326010+jcopaiva@users.noreply.github.com>
2021-10-22 15:07:56 +0100

    Add files via upload

---
+++ WindowsFormsApp1/WindowsFormsApp1/EventosNormal.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class EventosNormal : Form
+    {
+        ServicoCentralWS.PessoaS username;
+
+        public EventosNormal(ServicoCentralWS.PessoaS user)
+        {
+            username = user;
+            InitializeComponent();
+        }
+
+        private void EventosNormal_Load(object sender, EventArgs e)
+        {
+            ServicoCentralWS.CentralServicoSoapClient aux = new ServicoCentralWS.CentralServicoSoapClient();
+            ServicoCentralWS.EventoS[] listaE = aux.ResListaEventos();
+
+            // funcao para popular comboBox1 (lista de eventos)
+            for (int i = 0; i < listaE.Length; i++)
+            {
+                if (listaE[i].Lotacao >= listaE[i].Capacidade) // ver se está cheio
+                {
+                    this.comboBox1.Items.Add(listaE[i].Nome + " - Evento Cheio");
+                }
+                else if (listaE[i].Capacidade == 0) // ver se foi cancelado
+                {
+                    this.comboBox1.Items.Add(listaE[i].Nome + " - Evento Indisponível");
+                }
+                else
+                {                 
+                    this.comboBox1.Items.Add(listaE[i].Nome);
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ServicoCentralWS.CentralServicoSoapClient aux = new ServicoCentralWS.CentralServicoSoapClient();
+            ServicoCentralWS.EventoS[] listaE = aux.ResListaEventos();
+            var eventoEscolhido = new ServicoCentralWS.EventoS();
+
+            string nomeAux = comboBox1.SelectedItem.ToString();
+
+            for (int i = 0; i < listaE.Length; i++)
+            {
+                if (nomeAux.Contains(listaE[i].Nome)) // encontrar o evento correspondente
+                {
+                    eventoEscolhido = listaE[i];
+                }
+            }
+
+            VerEventoN obj = new VerEventoN(username, eventoEscolhido);
+            obj.Show();
+            this.Hide();
+        }
+
+    }
+}
---
+++ WindowsFormsApp1/WindowsFormsApp1/Login.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e) // botao login
+        {
+            ServicoCentralWS.CentralServicoSoapClient aux = new ServicoCentralWS.CentralServicoSoapClient();
+            string user = textBox1.Text;
+            string pass = textBox2.Text;
+            ServicoCentralWS.PessoaS pessoa = aux.ResultadoLogin(user, pass);
+            if (pessoa.Tipo == 1)
+            {
+                MessageBox.Show("Login efetuado com sucesso.");
+                PainelAdmin obj = new PainelAdmin(pessoa);
+                obj.Show();
+                this.Hide();
+            }
+            else if (pessoa.Tipo == 0)
+            {
+                EventosNormal obj = new EventosNormal(pessoa);
+                obj.Show();
+                this.Hide();
+            }
+            else
+            {
+                MessageBox.Show(pessoa.Nome, "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void Button2_Click(object sender, EventArgs e) // botao registo
+        {
+            Signup obj = new Signup();
+            obj.Show();
+            this.Hide();
+        }
+
+        private void textBox1_KeyDown(object sender, KeyEventArgs e) // quando se dá enter na textbox do usernmae, passa para a outra textbox
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                textBox2.Focus();
+            }
+        }
+
+        private void textBox2_KeyDown(object sender, KeyEventArgs e) // quando se dá enter na textbox da password, chama o botao login
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                Button1_Click(sender, e);
+            }
+        }
+
+    }
+}
